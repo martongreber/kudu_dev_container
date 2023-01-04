74cce8db0834300073c6a4ad3026555c63dfdb92
martongreber <greber.mX@gmail.com>
2023-01-04 19:36:35 +0100

    added proper remote caching for buildx

--- docker/build-and-publish.sh
+++ docker/build-and-publish.sh
@@ -3,7 +3,8 @@
 # exit when any command fails, to avoid long hanging job
 set -e
 build_arch=$(uname -m)
-build_types=("dev" "kudu-thirdparty" "kudu-thirdparty-all" "kudu-debug" "kudu-release" "kudu-asan" "kudu-tsan")
+build_types=("dev" "kudu-thirdparty-intermediate" "kudu-thirdparty" "kudu-thirdparty-all" "kudu-debug" "kudu-release" "kudu-asan" "kudu-tsan")
+cache_for_type=("" "dev" "kudu-thirdparty-intermediate" "kudu-thirdparty-intermediate" "kudu-thirdparty" "kudu-thirdparty" "kudu-thirdparty-all" "kudu-thirdparty-all")
 builder_name="insecure_builder"
 username="murculus"
 
@@ -31,15 +32,29 @@ build-and-publish() {
 
   echo "$(timestamp) LOG: pwd: $(pwd)"
 
-  for build_type in "${build_types[@]}"
+  cache_switches=""
+  for index in "${!build_types[@]}"
   do
-      echo "$(timestamp) LOG: starting image build: $build_type:$build_arch"
-      time docker buildx build --output type=docker --allow security.insecure --target $build_type -t murculus/$build_type:$build_arch . 
-      echo "$(timestamp) LOG: finished image build: $build_type:$build_arch"
+      build_type=${build_types[$index]}
+      echo "$(timestamp) LOG: creating cache switches at build stage $build_type:$build_arch" 
+      if [ ! -z "${cache_for_type[$index]}" ]; then
+        cache_switches=" --cache-from=type=registry,ref=$username/${cache_for_type[$index]}:$build_arch"
+      else
+        cache_switches=""
+      fi
+      cache_switches+=" --cache-from=type=registry,ref=$username/$build_type:$build_arch"
+      echo "$(timestamp) LOG: cache switches: $cache_switches"
 
-      echo "$(timestamp) LOG: starting image push to dockerhub: $build_type:$build_arch"
-      time docker push murculus/$build_type:$build_arch
-      echo "$(timestamp) LOG: finished image push to dockerhub: $build_type:$build_arch"
+      echo "$(timestamp) LOG: starting image build: $build_type:$build_arch"
+      set -x
+      time docker buildx build --push \
+                              --allow security.insecure \
+                              --cache-to=type=inline \
+                              $cache_switches \
+                              --target $build_type \
+                              -t murculus/$build_type:$build_arch . 
+      set +x
+      echo "$(timestamp) LOG: finished image build: $build_type:$build_arch"
 
       #update the manifest
       echo "$(timestamp) LOG: starting updating the manifest, latest tag: $build_type:$build_arch"
